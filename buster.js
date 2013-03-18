2d0f11a4e80e17e84a921c57a0fde1d05e88c168
Joakim Ohlrogge <joakim.ohlrogge@agical.com>
2013-03-18 19:45:23 +0100

    initial commit

---
+++ buster.js
@@ -0,0 +1,17 @@
+var config = module.exports;
+
+config["browser tests"] = {
+    environment: "browser",
+    sources: ["bud*.js",
+              "modules/foliage/foliage.js",
+              "modules/phloem/phloem.js",
+              "modules/lodash/lodash.js",
+              "modules/when/**/*.js"
+             ],
+    tests: ["test/*.js"],
+    libs: ["modules/curl/src/curl.js",
+           "loaderconf.js",
+           "ext/*.js"],
+    extensions: [require("buster-amd")]
+};
+
