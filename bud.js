c93ea193eb94e7554d8053c9e278db4ecd20a163
Joakim Ohlrogge <joakim.ohlrogge@gmail.com>
2013-05-16 13:55:07 +0300

    Update bud.js
    Added match to be able to chose different views based on inputs

--- bud.js
+++ bud.js
@@ -1,24 +1,50 @@
 define(['phloem', 'foliage', 'jquery'], function(phloem, f, $) {
-    return {
-        scope: function(s) {
-            return s();
-        },
-        bind: function(stream, element) {
-            return function(parent) {
-                var last = {undo: function(){}};
-                phloem.each(stream, function(value) {
-                    last.undo();
-                    last = element(value)(parent);
-                });
-            }
-        },
-        bus: function(fn) {
-            var bus = {};
-            bus.expose = function(element) {
-                var jqelem = $(element);
-                bus[jqelem.attr('id')] = function(v){return  v ? jqelem.val(v) : jqelem.val()};
-            };
-            return f.all(fn(bus));
-        }
+  function bind (stream , element) {
+    return function(parent) {
+      var last = {undo: function(){}};
+      phloem.each(stream, function(value) {
+        last.undo();
+        last = element(value)(parent);
+      });
     }
+  }
+  return {
+    scope: function(s) {
+      return s();
+    },
+    bind: bind,
+    bus: function(fn) {
+      var bus = {};
+      bus.expose = function(element) {
+        var jqelem = $(element);
+        bus[jqelem.attr('id')] = function(v){return  v ? jqelem.val(v) : jqelem.val()};
+      };
+      return f.all(fn(bus));
+    },
+    match: function (stream) {
+      var cases = Array.prototype.slice.call (arguments, 1);
+      return bind (stream, function (value) {
+        var c;
+        for (c in cases) {
+          var result = cases [c] (value);
+          if (result) {
+            return result;
+          }
+        }
+        return f.span ();
+      })
+    },
+    when: function (matcher) {
+      return {
+        then: function (outcome) {
+          return function (value) {
+            var matched = matcher (value);
+            if (matched) {
+              return outcome (matched);
+            }
+          }
+        }
+      }
+    }
+  }
 });
