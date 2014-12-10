e0b5ebb7507582e2f5db3f3a06e010e8ecba7749
Joakim Ohlrogge <joakim.ohlrogge@agical.com>
2014-12-10 22:48:16 +0100

    removed bad ideas

--- bud.js
+++ bud.js
@@ -9,57 +9,6 @@
 	    });
         }
 
-        var iterate = function(iterator) {
-	    return {
-	        onEach:function(ui) {
-		    return function(parent) {
-		        var undo = {undo: function(){}};
-		        var current = doNext(iterator, ui, parent, undo);
-		        return undo;
-		    }
-	        }
-	    }
-        }
-
-        var aggregate = function(events, ui) {
-	    var identity = function(val) {return val};
-	    return function(parent) {
-	        var added = {};
-                var addToUI = function(valueToAdd) {
-                    added[identity(valueToAdd)] = ui(valueToAdd)(parent);
-                };
-
-	        phloem.each(events, function(value) {
-		    if(value) {
-		        if(value.snap) {
-                            _(added).values().invoke('undo');
-                            added = {};
-                            _.each(value.snap, addToUI);
-                        }
-                        _.each(value.added, addToUI);
-		        _.each(value.dropped, function(droppedValue) {
-			    var dropped = added[identity(droppedValue)];
-			    if(dropped)  {
-			        added[identity(droppedValue)] = undefined;
-			        dropped.undo();
-			    }
-		        })
-		            }
-	        })
-	            return {undo: function() {
-		        _.each(added, function(leaf){leaf.undo()});
-	            }}
-	    }
-        }
-
-        var NOOP = function(data){
-	    return function(parent){
-	        return {
-		    undo: function(){}
-	        }
-	    }
-        };
-
         function bind (stream, elementFactory, initial) {
             return function(parent) {
                 var update = parent.__dynamic(elementFactory, initial);
@@ -69,103 +18,10 @@
         }
 
         var res = {
-	    create: function(item) {
-	        var itemToShow = f.create(item);
-	        return function(data) {
-		    return itemToShow;
-	        }
-	    },
-	    display: function(item) {
-	        var item = item;
-	        return function(data) {
-		    return item;
-	        }
-	    },
-	    whenever: function(either) {
-	        var leftElement, rightElement;
-	        var parentElement;
-	        leftElement = rightElement = NOOP;
-	        var elementToPresent = function(parent){return parent};
-	        var done = [];
-	        var undo = function() {
-		    var i;
-		    for(i in done) {
-		        done[i].undo && done[i].undo();
-		    }
-	        }
-	        var attach = function(current, currentElement, next, nextElement) {
-		    when(current()).then(
-		        function(model) {
-			    undo();
-			    done = [];
-			    attach(next, nextElement, current, currentElement);
-			    elementToPresent = currentElement(model);
-			    done.push(elementToPresent(parentElement))
-		        }
-		    )
-	        }
-
-	        return {
-		    then: function(primary) {
-		        leftElement = primary;
-		        var result = function(parent) {
-			    parentElement = parent;
-			    elementToPresent(parent);
-			    attach(either.left, leftElement, either.right, rightElement);
-			    return {undo: undo}
-		        }
-		        
-		        result.otherwise = function(alternative) {
-			    rightElement = alternative;
-			    attach(either.right, rightElement, either.left, leftElement);
-			    return result;
-		        }
-		        return result;
-		    }
-	        }
-	    },
-	    iterate: iterate,
-	    aggregate: aggregate,
             scope: function(s) {
                 return s();
             },
-            bind: bind,
-            bus: function(fn) {
-                var bus = {};
-                bus.expose = function(element) {
-                    var jqelem = $(element);
-                    var id = jqelem.attr('id');
-                    bus [id] = function(v) {
-		        return (typeof v === "undefined") ? jqelem.val() : jqelem.val(v);
-		    };
-                    bus [id].element = jqelem;
-                };
-                return f.all(fn(bus));
-            },
-            match: function (stream) {
-                var cases = Array.prototype.slice.call (arguments, 1);
-                return bind (stream, function (value) {
-                    var c;
-                    for (c in cases) {
-                        var result = cases [c] (value);
-                        if (result) {
-                            return result;
-                        }
-                    }
-                    return f.span ();
-                })
-            },
-            when: function (matcher) {
-                return {
-                    then: function (outcome) {
-                        return function (value) {
-                            var matched = matcher (value);
-                            if (matched) {
-                            }
-                        }
-                    }
-                }
-            }
+            bind: bind
         }
         return res;
     }
@@ -181,5 +37,4 @@
             require('foliage/foliage'), 
             require('q'));
     }
-
 })();
--- test/bud-spec.js
+++ test/bud-spec.js
@@ -12,224 +12,58 @@
         
         var assert = spec.assert;
 
-        var scope = spec("scope", {
-            "returned element gets displayed" : withContext(function(ctx){
-                var result = f.div(b.scope(
-                    function() {
-                        var message = "hello " + "world";
-                        return f.p("#somep", message);
+        var scope = spec("scope",
+                         {
+                             "returned element gets displayed" : withContext(function(ctx){
+                                 var result = f.div(b.scope(
+                                     function() {
+                                         var message = "hello " + "world";
+                                         return f.p("#somep", message);
+                                     }
+                                 ))(ctx);
+                                 return assert.equals(ctx.text(ctx.find(result, "div #somep")).trim(), "hello world");
+                             })
+                         });
+
+        var bind = spec("bind").
+            preconditions(
+                function getDom(){return dom;}
+            )(
+                {
+                    "no value no initial" : function(ctx){
+                        var stream = consfn.forArray([]);
+                        var result = f.div(b.bind(stream, f.p, "initial"))(ctx);
+                        return assert.equals(ctx.text(ctx.find(result, "div p")).trim(), "initial");
+                    },
+                    "creates element from pushed value" : function(ctx){
+                        var stream = consfn.forArray(['first value']);
+                        var result = f.div(b.bind(stream, f.p, "initial"))(ctx);
+
+                        return when(stream.next()).then(function(value){
+                            return assert.equals(
+                                ctx.text(ctx.find(result, "div p")).trim(),
+                                consjs.value(value));
+                        });
+                    },
+                    "keeps only last pushed value" : function(ctx){
+                        var stream = consjs.stream();
+                        var result = f.div(b.bind(stream.read, f.p))(ctx);
+                        var next = stream.read.next();
+                        stream.push('first');
+                        return when(next).then(function(value){
+                            var paraText = ctx.text(ctx.find(result, "div p")).trim();
+                            return assert.equals(paraText, consjs.value(value)).
+                                then(function(){ stream.push("second");}).
+                                then(function(){return consjs.next(value)});
+                        }).then(
+                            function(value) {
+                                var paraText = ctx.text(ctx.find(result, "div p")).trim();
+                                return assert.equals(paraText, consjs.value(value))
+                            }
+                        );
                     }
-                ))(ctx);
-                
-                assert.equals(ctx.text(ctx.find(result, "div #somep")).trim(), "hello world");
-            })
-        });
-
-        var bind = spec("bind", {
-            "no value no initial" : withContext(function(ctx){
-                var stream = consfn.forArray([]);
-                var result = f.div(b.bind(stream, f.p, "initial"))(ctx);
-                assert.equals(ctx.text(ctx.find(result, "div p")).trim(), "initial");
-            }),
-            "creates element from pushed value" : withContext(function(ctx){
-                var stream = consfn.forArray(['first value']);
-                var result = f.div(b.bind(stream, f.p, "initial"))(ctx);
-
-                return when(stream.next()).then(function(value){
-                    assert.equals(
-                        ctx.text(ctx.find(result, "div p")).trim(),
-                        "pushed value");
                 });
-            }),
-            "keeps only last pushed value" : withContext(function(ctx){
-                var stream = consjs.stream();
-                var result = f.div(b.bind(stream.read, f.p))(ctx);
-                var next = stream.read.next();
-                stream.push('first');
-                return when(next).then(function(value){
-                    var paraText = ctx.text(ctx.find(result, "div p")).trim();
-                    return assert.equals(paraText, "first").
-                        then(function(){ stream.push("second");}).
-                        then(function(){return consjs.next(value)});
-                }).then(
-                    function(value) {
-                        var paraText = ctx.text(ctx.find(result, "div p")).trim();
-                        return assert.equals(paraText, "second")
-                    }
-                );
-
-                
-            })
-        });
-
-        spec("bus", {
-            "can expose value": withContext(function(ctx) {
-                b.bus(function(bus) {
-                    return f.all(
-                        f.input("#source", {value: "from source"}, bus.expose),
-                        f.input("#target", {value: "unchanged"}, bus.expose,
-                                on.click(function() {
-                                    bus.target(bus.source());
-                                })));
-                })(ctx);
-                assert.equals($('#target', ctx).val().trim(), "unchanged");
-                $("#target", ctx).click();
-                assert.equals($('#target', ctx).val().trim(), "from source");
-            }),
-            "can set falsey value like empty string": withContext(function(ctx) {
-                b.bus(function(bus) {
-                    return f.all(
-                        f.input("#target", {value: "unchanged"}, bus.expose,
-                                on.click(function() {
-                                    bus.target("");
-                                })));
-                })(ctx);
-                assert.equals($('#target', ctx).val().trim(), "unchanged");
-                $("#target", ctx).click();
-                assert.equals($('#target', ctx).val().trim(), "");
-            })
-        });
-
-        var iterateSetup = function() {
-	    var queue = phloem.queue();
-	    var component = b.iterate(queue.next()).onEach(
-	        function(value) {
-		    return f.p("value: ", value[0]);
-	        }
-	    );
-
-
-	    var context = $('<div />');
-	    component(context);
-	    return {
-	        context: context,
-	        queue: queue
-	    }
-        }
-
-
-        spec("iterate", {
-	    "adds one element per iteration" : function() {
-	        var setup = iterateSetup();
-
-	        return when("a value").
-		    then(setup.queue.push).
-		    then(function() {
-		        assert.equals(setup.context.children().text().trim(), "value: \na value");
-		    });
-	    }
-        });
-
-        function setUpAggregate() {
-	    var eventStream = phloem.events(phloem.stream());
-	    var component = b.aggregate(eventStream.read.next(),
-	                                function(value) {
-		                            return f.p(value + " value", {id: value})
-	                                }
-	                               );
-
-
-	    var context = $('<div />');
-	    var aggregate = component(context);
-	    return {
-	        context : context,
-	        eventStream: eventStream,
-	        aggregate: aggregate
-	    };
-        }
-
-        spec("aggregate", {
-	    "added element is displayed" : function() {
-	        var setUp = setUpAggregate();
-	        var promise =  when(setUp.eventStream.read.next()).
-		    then(function(value) {
-		        assert.equals($('#item1', setUp.context).text().trim(), "item1 value");
-		    })
-	        setUp.eventStream.push("item1");
-	        return promise;
-	    },
-            "snapshot is displayed": function() {
-	        var setUp = setUpAggregate();
-	        var promise = when(setUp.eventStream.read.next()).
-		    then(yield).
-                    then(function(value) {
-		        assert.equals($('#item1', setUp.context).text().trim(), "item1 value");
-		        assert.equals($('#item2', setUp.context).text().trim(), "item2 value");
-                    });
-
-	        setUp.eventStream.snap(["item1", "item2"]);
-	        return promise;            
-            },
-            "snapshot overrides previous state": function() {
-	        var setUp = setUpAggregate();
-	        var promise = when(setUp.eventStream.read.next()).
-                    then(phloem.next).
-                    then(yield).
-		    then(function(value) {
-                        refute.equals($('#item0', setUp.context).text().trim(), "item0 value");
-		        assert.equals($('#item1', setUp.context).text().trim(), "item1 value");
-		        assert.equals($('#item2', setUp.context).text().trim(), "item2 value");
-		    })
-
-                setUp.eventStream.push("item0");
-	        setUp.eventStream.snap(["item1", "item2"]);
-	        return promise;            
-            },                
-	    "two added elements in separate events are displayed" : function() {
-	        var setUp = setUpAggregate();
-	        var promise = when(setUp.eventStream.read.next()).
-		    then(phloem.next).
-                    then(yield).
-		    then(function(value) {
-		        assert.equals($('#item1', setUp.context).text().trim(), "item1 value");
-		        assert.equals($('#item2', setUp.context).text().trim(), "item2 value");
-		    })
-
-	        setUp.eventStream.push("item1");
-	        setUp.eventStream.push("item2");
-	        return promise;
-
-	    },
-	    "added element can be undone" : function() {
-	        var setUp = setUpAggregate();
-	        var promise =  when(setUp.eventStream.read.next()).
-                    then(function(value){
-                        console.log("value0", 0);
-                        return value;
-                    }).
-		    then(function(value) {
-                        console.log("value1 ", value);
-		        assert.equals($('#item1', setUp.context).text().trim(), "item1 value");
-		        setUp.aggregate.undo();
-                        return value;}).
-                    then(yield).
-                    then(function(value){
-                        console.log("value2 ", value);
-		        assert.equals($('#item1', setUp.context).text().trim(), "");
-		    });
-	        setUp.eventStream.push("item1");
-	        return promise;
-	    },
-	    "two added elements and one removed displays one added" : function() {
-	        var setUp = setUpAggregate();
-	        var promise = when(setUp.eventStream.read.next()).
-		    then(phloem.next).
-		    then(phloem.next).
-		    then(function(value) {
-		        assert.equals($('#item1', setUp.context).text().trim(), "item1 value");
-		        assert.equals($('#item2', setUp.context).text().trim(), "");
-		    })
-
-	        setUp.eventStream.push("item1");
-	        setUp.eventStream.push("item2");
-	        setUp.eventStream.drop("item2");
-	        return promise;
-
-	    }
-        });
-
         return [scope, bind];
-
     }
     if (typeof define !== 'undefined') {
         define(['tattler/spec', 
